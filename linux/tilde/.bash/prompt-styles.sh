80e49c28741c0ba7c1a0295ca45c9016b773683b
valentin fleurit <vfleurit@gmail.com>
2022-04-27 16:28:02 +0200

    edit prompt-styles.sh

--- linux/tilde/.bash/prompt-styles.sh
+++ linux/tilde/.bash/prompt-styles.sh
@@ -73,12 +73,15 @@ execStatus() {
     fi
 }
 
-prompt1="\n \[$(fgc 0 && bgc 5)\] \W \[$(reset)\] - "
-prompt7="\n \[$(bgc 5 && fgc 0 && bold)\] \u \[$(reset && bgc 2 && fgc 5)\]◤\[$(fgc 0 && bgc 2)\] \W \[$(bgc 4 && fgc 2)\]◤\[$(fgc 0 && bgc 4)\] \$(fileAmount) items \[$(reset)\] - "
-prompt2="\n \[\e[0;33m\]◢\[\e[43;30m\] \W \[\e[0;33m\]◤\[\e[0m\] \[\e[34m\]➜\[\e[0m\]  "
-prompt3="\n ╭  \[\e[35m\]$(bold)\W$(reset) \[\e[34m\]・\[\e[0m\] \[\e[33m\]$(bold)\$(fileAmount)$(reset)\[\e[0m\] files → \[\e[33m\]$(bold)$(dirSize)B$(reset)\[\e[0m\] \[\e[34m\]・\[\e[0m\] \[\e[32m\]$(bold)$(battery)%$(reset)\[\e[0m\]\n │\n ╰  "
-prompt4="\n \[\e[0;33m\]◢\[\e[43;30m\] \h \[\e[44;33m\]◤\[\e[44;30m\] \u \[\e[45;34m\]◤\[\e[45;30m\] \W \[\e[47;35m\]◤\[\e[47;30m\] $(git_branch) \[\e[0;37m\]◤\[\e[0m\] "
-prompt5="╭ \w\n├ \u ─ \A\n│\n╰ "
-prompt6="\[\033[00m\] \n\[\033[30;45m\] \T \[\033[00m\]\[\033[30;46m\] \h@\u \[\033[00m\]\[\033[37;44m\] \w \[\033[1;00m\] "
+customPwd() {
+    pwd | sed -e "s/\// /g"
+}
 
-PS1=$prompt7
+style1="\n \[$(bgc 5 && fgc 0 && bold)\] \u \[$(reset && bgc 2 && fgc 5)\]◤\[$(fgc 0 && bgc 2)\] \W \[$(bgc 4 && fgc 2)\]◤\[$(fgc 0 && bgc 4)\] \$(fileAmount) items \[$(reset)\] › "
+style2="\n \[$(fgc 0 && bgc 2)\] \W \[$(reset)\] - "
+style3="\n \[\e[0;33m\]◢\[\e[43;30m\] \W \[\e[0;33m\]◤\[\e[0m\] \[\e[34m\]➜\[\e[0m\]  "
+style4="\n \[\e[0;33m\]◢\[\e[43;30m\] \h \[\e[44;33m\]◤\[\e[44;30m\] \u \[\e[45;34m\]◤\[\e[45;30m\] \W \[\e[47;35m\]◤\[\e[47;30m\] $(git_branch) \[\e[0;37m\]◤\[\e[0m\] "
+style5="\n \[$(bold)\]\u\[$(reset)\] › "
+style6="\n \[$(bold)\]\u\[$(reset)\] λ "
+
+PS1=$style1
