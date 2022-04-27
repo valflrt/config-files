44f7d96357fb3c81630b11bbfe107f6eaea67a29
valentin fleurit <vfleurit@gmail.com>
2022-04-27 14:26:35 +0200

    move copiable files to "tilde/"

--- linux/.bash/init-sequence.sh
+++
@@ -1,2 +0,0 @@
-# init sequence
-clear # && keqing
---
+++ linux/tilde/.bash/functions-and-aliases.sh
@@ -0,0 +1,10 @@
+# some aliases
+alias la='ls -A'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+mkcd() {
+  mkdir "$1" && cd "$1"
+}
